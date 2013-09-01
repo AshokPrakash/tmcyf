9c11ad6fd7a5687467212cf986061b0e98895b90
ivanvarghese <ivanvarghese@me.com>
2013-08-31 23:46:26 -0500

    Configured Nginx + Unicorn to serve site over SSL.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,7 @@ Static::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.force_ssl = true
 
   PUBLIC_URL = "http://localhost:3000/"
 
